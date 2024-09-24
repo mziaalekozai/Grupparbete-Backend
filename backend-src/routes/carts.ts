febc7d3105ea3e1e99224884fc5561f46ce15838
Grunkans <143597576+Grunkans@users.noreply.github.com>
2024-09-24 20:44:01 +0200

    fixat allInCart

---
+++ backend-src/routes/carts.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response, Router } from "express";
+import {Carts} from "../models/cart.js";
+import { WithId } from "mongodb";
+import { getAllInCart } from "../database/cart.js";
+
+export const router: Router = express.Router();
+
+router.get("/", async (req: Request, res: Response<WithId<Carts>[]>) => {
+  const allInCart: WithId<Carts>[] = await getAllInCart();
+  res.send(allInCart);
+});
--- backend-src/server.ts
+++ backend-src/server.ts
@@ -1,6 +1,7 @@
 import express, { Express, NextFunction, Request, Response } from "express";
 import { router as productsRouter } from "./routes/products.js";
 import { router as usersRouter } from "./routes/users.js";
+import { router as cartRouter } from "./routes/carts.js"
 
 const app: Express = express();
 const port = 3333;
@@ -13,6 +14,8 @@ app.use("/", (req: Request, res: Response, next: NextFunction) => {
 app.use("/products", productsRouter);
 
 app.use("/users", usersRouter);
+
+app.use("/cart", cartRouter);
 
 app.listen(port, () => {
   console.log("Server is listening on port " + port);
